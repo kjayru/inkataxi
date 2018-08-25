8a2079fc2a57cf039e9927e7103a5ab74b2276cf
Wile TINOCO YURIVILCA <wiltinoco@iMac-de-Wile.local>
2018-08-25 04:31:22 -0500

    detalles

--- resources/views/vendor/voyager/cliente/viajes.blade.php
+++ resources/views/vendor/voyager/cliente/viajes.blade.php
@@ -3,7 +3,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 @stop
 @section('content')
-
+<section class="content-header">
+        <h1>Viaje realizado</h1>
+    </section>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
